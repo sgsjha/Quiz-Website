90c5fbe1cf59a0985ca40d6b2214a063398c15eb
Sarthak Jha <sgsjha@liverpool.ac.uk>
2024-06-17 22:18:53 +0530

    fix

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -1,11 +1,23 @@
-// src/StartPage.js
 import React from 'react';
 
 const StartPage = ({ onStart }) => {
+  const handleClick = () => {
+    const quizState = localStorage.getItem('currentQuestion');
+    if (quizState !== null && quizState !== undefined) {
+      // Quiz state found in localStorage, continue quiz
+      onStart(true); // Pass true to indicate continuing quiz
+    } else {
+      // No quiz state found, start new quiz
+      onStart(false); // Pass false to indicate starting new quiz
+    }
+  };
+
   return (
     <div>
       <h1>Welcome to the Country Capitals Quiz</h1>
-      <button onClick={onStart}>Start Quiz</button>
+      <button onClick={handleClick}>
+        {localStorage.getItem('currentQuestion') ? 'Continue Quiz' : 'Start Quiz'}
+      </button>
     </div>
   );
 };
